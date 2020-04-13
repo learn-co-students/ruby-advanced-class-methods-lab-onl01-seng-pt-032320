9b5f01f2d4bb6c922979f1026a86776793188bca
Evan Smith <professionalemail@zoho.com>
2020-04-13 20:26:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,35 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|person| person.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.all.include?(name)
+      self.find_by_name(name)
+     else
+      self.create_by_name(name)
+    end
+  end
+  
 end
