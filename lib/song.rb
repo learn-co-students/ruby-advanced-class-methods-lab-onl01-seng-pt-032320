86bce943fa2b240488b577e8f15c992edc11d3cc
Evan Smith <professionalemail@zoho.com>
2020-04-14 01:24:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -34,11 +36,45 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if self.all.include?(name)
-      self.find_by_name(name)
+    finder = find_by_name(name)
+    if finder != nil
+      finder
      else
       self.create_by_name(name)
     end
+  end
+  
+  def self.alphabetical
+    self.all.sort_by &:name
+  end
+  
+  def self.new_from_filename(file)
+    raw_data = file.split("-")
+    artist = raw_data[0].strip
+    raw_data = raw_data[1]
+    raw_data = raw_data.split(".")
+    name = raw_data[0].strip
+    song = self.new 
+    song.name = name
+    song.artist_name = artist
+    song
+  end
+  
+  def self.create_from_filename(file)
+    raw_data = file.split("-")
+    artist = raw_data[0].strip
+    raw_data = raw_data[1]
+    raw_data = raw_data.split(".")
+    name = raw_data[0].strip
+    song = self.new 
+    song.name = name
+    song.artist_name = artist
+    self.all << song
+    song
+  end
+  
+  def self.destroy_all
+    self.all.clear
   end
   
 end
